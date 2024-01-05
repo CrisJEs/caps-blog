c8963c1e06d1d958bb2ae5b554e0a30130256fa4
Miguel Reina <miguel.reina@trade.ec>
2024-01-05 17:05:43 -0500

    feat(hamburguer): logic

--- src/components/Hambuguer.tsx
+++ src/components/Hambuguer.tsx
@@ -1,12 +1,28 @@
 import React from "react";
+import "./Hamburguer.css";
 
 const Hamburguer = () => {
   return (
-    <div class="hamburger">
-      <span class="line"></span>
-      <span class="line"></span>
-      <span class="line"></span>
-    </div>
+    <>
+      <div class="hamburger" className="container">
+        {/* <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span> */}
+      </div>
+      {/* <nav class="navigation">
+        <ul>
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="/blog/">Blog</a>
+          </li>
+          <li>
+            <a href="/about/">Acerca de</a>
+          </li>
+        </ul>
+      </nav> */}
+    </>
   );
 };
 
